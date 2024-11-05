867ad37f64919efcbc7252e78d1c12faa8a973a8
Batuhan YALIN <batuhanyalin2@gmail.com>
2024-11-05 19:06:03 +0300

    Mail gönderme sistemi kuruldu.

---
+++ Frontend/HotelApiProject.WebUI/Areas/Admin/Controllers/MailController.cs
@@ -0,0 +1,51 @@
+﻿using HotelApiProject.WebUI.Models.Mail;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+
+namespace HotelApiProject.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/[controller]")]
+    public class MailController : Controller
+    {
+        [HttpGet]
+        [Route("Index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [Route("Index")]
+        public IActionResult Index(MailViewModel model)
+        {
+            MimeMessage mimeMessage = new MimeMessage();
+
+            //Kimden 
+            MailboxAddress mailboxAddressFrom = new MailboxAddress("Hotel Api - Admin", "hotelapi@batuhanyalinsoftware.com.tr");
+            mimeMessage.From.Add(mailboxAddressFrom);
+
+            //Kime 
+            MailboxAddress mailboxAddressTo = new MailboxAddress("User", model.ReceiverMail);
+            mimeMessage.To.Add(mailboxAddressTo);
+            //Mesaj içeriği 
+            var bodyBuilder = new BodyBuilder();
+            bodyBuilder.TextBody = model.Body;
+
+            mimeMessage.Subject = model.Subject;
+            mimeMessage.Body = bodyBuilder.ToMessageBody();
+            mimeMessage.Date = model.SendingDate;
+            //Smtp Ayarları (MailKitSmtp)
+            SmtpClient client = new SmtpClient();
+            //Smtp Bağlantı Adresi
+            SmtpClient smtpClient = new SmtpClient();
+            smtpClient.ServerCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true;
+            smtpClient.Connect("mail.batuhanyalinsoftware.com.tr", 465, SecureSocketOptions.SslOnConnect);
+            smtpClient.Authenticate("hotelapi@batuhanyalinsoftware.com.tr", "3rNa@023c");
+            smtpClient.Send(mimeMessage);
+            smtpClient.Disconnect(true);
+            return View();
+        }
+    }
+}
---
+++ Frontend/HotelApiProject.WebUI/Models/Mail/MailViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace HotelApiProject.WebUI.Models.Mail
+{
+    public class MailViewModel
+    {
+        public string Name { get; set; }
+        public string SenderMail { get; set; }
+        public string ReceiverMail { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public DateTime SendingDate { get; set; }
+    }
+}
