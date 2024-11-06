c7aa13b9eecd9166ff43e5ddd5cd9aae630a2d61
Batuhan YALIN <batuhanyalin2@gmail.com>
2024-11-06 07:46:10 +0300

    API ile API FileUpload ve API FileConsume işlemleri yapıldı.

---
+++ ApiConsume/HotelApiProject.WebApi/Controllers/FileUploadController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelApiProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileUploadController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> UploadImage([FromForm]IFormFile file)
+        {
+            var fileName= Guid.NewGuid()+Path.GetExtension(file.FileName);
+            var path= Path.Combine(Directory.GetCurrentDirectory(), "uploads/"+fileName);
+            var stream = new FileStream(path, FileMode.Create);
+            await file.CopyToAsync(stream);
+            return Created("",file);
+        }
+    }
+}
---
+++ Frontend/HotelApiProject.WebUI/Areas/Admin/Controllers/FileController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+namespace HotelApiProject.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/[controller]")]
+    public class FileController : Controller
+    {
+        [Route("Index")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("Index")]
+        [HttpPost]
+        public async Task<IActionResult> Index(IFormFile file)
+        {
+            var stream = new MemoryStream();
+            await file.CopyToAsync(stream);
+            var bytes = stream.ToArray();
+
+            ByteArrayContent byteArrayContent = new ByteArrayContent(bytes);
+            byteArrayContent.Headers.ContentType = new MediaTypeHeaderValue(file.ContentType);
+            MultipartFormDataContent content = new MultipartFormDataContent();
+            content.Add(byteArrayContent, "file", file.FileName);
+            var httpClient = new HttpClient();
+            await httpClient.PostAsync("http://localhost:5173/api/FileUpload", content);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
