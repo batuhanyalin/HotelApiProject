afe4d721ea75b21b1f50abfe078a6e9549847c51
Batuhan YALIN <batuhanyalin2@gmail.com>
2024-11-09 16:16:10 +0300

    DashboardController oluşturuldu ve viewComponentlere ayrıldı.

---
+++ Frontend/HotelApiProject.WebUI/Areas/Admin/ViewComponents/_DashboardCustomersComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelApiProject.WebUI.Areas.Admin.ViewComponents
+{
+    public class _DashboardCustomersComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ Frontend/HotelApiProject.WebUI/Areas/Admin/ViewComponents/_DashboardSalesActivitiesComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelApiProject.WebUI.Areas.Admin.ViewComponents
+{
+    public class _DashboardSalesActivitiesComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
