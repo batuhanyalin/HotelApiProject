cc1963dc59d1d0876bb5289ff3db1a209dd88bbc
Batuhan YALIN <batuhanyalin2@gmail.com>
2024-11-11 11:23:02 +0300

    Dashboard tamamlandı.

--- Frontend/HotelApiProject.WebUI/Areas/Admin/ViewComponents/_DashboardSocialMediaLinkedinComponentPartial.cs
+++ Frontend/HotelApiProject.WebUI/Areas/Admin/ViewComponents/_DashboardSocialMediaLinkedinComponentPartial.cs
@@ -1,12 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelApiProject.WebUI.Dtos.DashboardDtos.SocialMedia;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace HotelApiProject.WebUI.Areas.Admin.ViewComponents
 {
     public class _DashboardSocialMediaLinkedinComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        public async Task<IViewComponentResult> InvokeAsync()
         {
-            return View();
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://fresh-linkedin-profile-data.p.rapidapi.com/get-linkedin-profile?linkedin_url=https%3A%2F%2Fwww.linkedin.com%2Fin%2Fbatuhanyalin%2F&include_skills=false&include_certifications=false&include_publications=false&include_honors=false&include_volunteers=false&include_projects=false&include_patents=false&include_courses=false&include_organizations=false&include_profile_status=false&include_company_public_url=false"),
+                Headers =
+    {
+        { "x-rapidapi-key", "87ebda1640msh1f0710fdc6ebb78p11788djsnc07507e0fe79" },
+        { "x-rapidapi-host", "fresh-linkedin-profile-data.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                LinkedinFollowersDto linkedinFollowersDto = JsonConvert.DeserializeObject<LinkedinFollowersDto>(body);
+                return View(linkedinFollowersDto);
+            }
+         
         }
     }
 }
---
+++ Frontend/HotelApiProject.WebUI/Dtos/DashboardDtos/SocialMedia/LinkedinFollowersDto.cs
@@ -0,0 +1,12 @@
+﻿namespace HotelApiProject.WebUI.Dtos.DashboardDtos.SocialMedia
+{
+    public class LinkedinFollowersDto
+    {
+        public Data data { get; set; }
+        public class Data
+        {
+            public int follower_count { get; set; }
+
+        }
+    }
+}
